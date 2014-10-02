d67423af4d3ed8aba1e58f7afefa9ce2c5c0e08c
John Sposato <jsposato@ufl.edu>
2014-10-01 21:18:54 -0400

    Added generators and migration for users table.  Configured local database

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
-			'password'  => 'secret',
+			'database'  => 'vehiclelog',
+			'username'  => 'vehiclelog',
+			'password'  => 'ch4ng3m3;',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
