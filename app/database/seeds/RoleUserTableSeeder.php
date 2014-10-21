fe7462133991da4b72a40f13f9525780685b58d9
John Sposato <jsposato@gmail.com>
2014-10-21 19:21:20 -0400

    Added roles and pivot, created seed files for one user

---
+++ app/database/seeds/RoleUserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+    use Faker\Factory as Faker;
+
+    class RoleUserTableSeeder extends Seeder
+    {
+
+        public function run()
+        {
+            $faker = Faker::create();
+
+            RoleUser::create(
+                [
+                    'role_id' => 1,
+                    'user_id' => 1
+                ]
+            );
+        }
+
+    }
---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+    use Faker\Factory as Faker;
+
+    class UsersTableSeeder extends Seeder
+    {
+
+        public function run()
+        {
+            $faker = Faker::create();
+
+            User::create(
+                [
+                    'username' => 'jsposato',
+                    'email'    => 'jsposato@gmail.com',
+                    'password' => Hash::make( 'codiesassy' )
+                ]
+            );
+        }
+
+    }
